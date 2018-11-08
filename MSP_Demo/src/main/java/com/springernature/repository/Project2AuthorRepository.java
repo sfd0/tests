51e0e532afeced958fe3ae96db99555e8d9b8ecf
Greg Safford <Greg Safford@LAUSNESP00-0671.springernature.com>
2018-11-08 12:13:57 -0500

    Dev.

--- MSP_Demo/src/main/java/com/springernature/repository/Project2AuthorRepository.java
+++ MSP_Demo/src/main/java/com/springernature/repository/Project2AuthorRepository.java
@@ -3,11 +3,13 @@
  */
 package com.springernature.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.springernature.domain.Project2Author;
 
 /**
  *
  */
-public interface Project2AuthorRepository extends Repository<Project2Author> {
+public interface Project2AuthorRepository extends JpaRepository<Project2Author, Long> {
 
 }
--- MSP_Demo/src/main/java/com/springernature/repository/ProjectRepository.java
+++ MSP_Demo/src/main/java/com/springernature/repository/ProjectRepository.java
@@ -3,11 +3,22 @@
  */
 package com.springernature.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.springernature.domain.ProjectEntity;
 
 /**
  *
  */
-public interface ProjectRepository extends Repository<ProjectEntity> {
+public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
+
+    /**
+     * Get an entity by primary key.
+     *
+     * @param id
+     *            -- primary key.
+     * @return -- The entity.
+     */
+    ProjectEntity findById(long id);
 
 }
